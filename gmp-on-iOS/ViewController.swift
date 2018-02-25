eb075f7aae9bc957f0f744885cc9ca03ac14f918
NTT <NTT@Shunichi-no-MacBook-Air.local>
2018-02-26 05:39:48 +0900

    Initial Commit

---
+++ gmp-on-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gmp-on-iOS
+//
+//  Created by NTT on 2018/02/26.
+//  Copyright © 2018年 NTT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
