323d2b131c1e8caf31a48d8dd928780eaec88418
NTT <NTT@Shunichi-no-MacBook-Air.local>
2018-02-26 05:48:03 +0900

    add gmp

---
+++ GMP/umbrella-gmp.h
@@ -0,0 +1,1 @@
+#import <libgmp/gmp.h>
--- gmp-on-iOS/ViewController.swift
+++ gmp-on-iOS/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import GMP
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World!")
+        
+        var a = mpz_t()
+        
+        __gmpz_init(&a) // 初期化
+        // 文字列の形で任意精度整数をセットする
+        __gmpz_set_str(&a, "48410242354393281104234213124421033", 10)
+        
+        let label = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        label.text = String(cString: __gmpz_get_str(nil, 10, &a)!)
+        label.numberOfLines = 0
+        label.sizeToFit()
+        self.view.addSubview(label)
+        // 後処理
+        __gmpz_clear(&a)
     }
 
     override func didReceiveMemoryWarning() {
